f0e11572980f045025137425fbb893660dad8af7
Попов Сергей Николаевич <lord_wizard@mail.ru>
2022-11-22 18:17:00 +0300

    End write program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество слов");
+int countmax=int.Parse(Console.ReadLine());
+string []array= new string[countmax];
+for (int count=0;count<countmax;count++)
+{
+Console.WriteLine("Введите слово №" + (count+1));
+array[count]=Console.ReadLine();
+}
+bool first=true;
+for (int count=0;count<countmax;count++)
+{
+    if (array[count].Length<=3)
+    {
+        if (first==true) 
+        {
+            Console.Write("[\""+array[count] +"\"");
+            first=false;
+        }else  Console.Write(", \""+array[count]+"\"" );   
+    }
+    
+
+}
+if (first==false) Console.Write("]");
+
